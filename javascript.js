0c962af204c82bf08f8362e001281470b77596e9
Shvan <shvan.yakana@student.hu.nl>
2025-03-10 10:58:36 +0100

    feat: add function to print grid of squares based on user input

--- javascript.js
+++ javascript.js
@@ -1,39 +1,42 @@
 const square = document.createElement("div");
 const fullPageContainer = document.querySelector(".containerAllContent");
 
-const fullPageLayout = () => {
+const fullPageLayout = (containerSize) => {
   fullPageContainer.setAttribute(
     "style",
-    "display:flex; flex-wrap: wrap; align-content: flex-start;"
+    `display:flex; flex-wrap: wrap; align-content: flex-start; max-width: ${containerSize}px; min-height: ${containerSize}px; max-height: ${containerSize}px;`
   );
 };
 
-const printGrid = () => {
+const userPrompt = () => {
   const n = prompt("how many squares");
-  const squareSize = calculateSquareSize(n);
+  printGrid(n);
+};
+
+const printGrid = (n) => {
   let i = 0;
-  while (i < n) {
+  while (i < n * n) {
+    const squareSize = calculateSquareSize(n);
     const newSquare = square.cloneNode(true);
     fullPageContainer.append(newSquare);
     newSquare.classList.toggle("single-square");
-
     newSquare.setAttribute(
       "style",
-      `display: flex; border: solid 1px black; min-width: ${squareSize}px; max-width: ${squareSize}px; min-height: ${squareSize}px; max-height:${squareSize}px; `
+      `display: flex; border: solid 1px black; min-width: ${squareSize}px; max-width: ${squareSize}px; min-height: ${squareSize}px; max-height:${squareSize}px; box-sizing: border-box`
     );
-
     i++;
   }
 };
 
 const calculateSquareSize = (nOfSquares) => {
   let containerSize = 520;
-  squareSize = containerSize / nOfSquares;
+  let squareSize = containerSize / nOfSquares;
   console.log(squareSize);
-  return Math.round(squareSize);
+  return squareSize;
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  fullPageLayout();
-  printGrid();
+  let containerSize = 520;
+  fullPageLayout(containerSize);
+  userPrompt();
 });
